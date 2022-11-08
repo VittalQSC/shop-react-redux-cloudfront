51f6c9f668abeb50b4ee5ff631da35d14c8043e7
Vitali Shatsou <vitshacov@gmail.com>
2022-11-08 21:22:04 +0100

    Task 6

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   products: "https://z7d95oanc0.execute-api.eu-west-1.amazonaws.com/dev",
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://cyg0rwmu8g.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
