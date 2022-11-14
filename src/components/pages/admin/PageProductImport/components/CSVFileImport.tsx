d19bdbdf2e26f9717636aedc46328e4f4febdd62
Vitali Shatsou <vitshacov@gmail.com>
2022-11-14 21:45:22 +0100

    Task 7

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -31,15 +31,20 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
     }
 
     // Get the presigned URL
+    const token = localStorage.getItem("authorization_token");
     const response = await axios({
       method: "GET",
       url,
+      headers: token ? { Authorization: `Basic ${token}` } : {},
       params: {
         name: encodeURIComponent(file.name),
       },
     });
     console.log("File to upload: ", file.name);
     console.log("Uploading to: ", response.data);
+    if (response.status === 401 || response.status === 403) {
+      alert(`The action was rejected: ${JSON.stringify(response)}`);
+    }
     const result = await fetch(response.data, {
       method: "PUT",
       body: file,
