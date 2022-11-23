0ff9c0fec5113d8ae99d8c87327e12cced28fbb6
Vitali Shatsou <vitshacov@gmail.com>
2022-11-23 06:50:20 +0100

    integrated with it Order model

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   products: "https://z7d95oanc0.execute-api.eu-west-1.amazonaws.com/dev",
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://so2zju5nw2.execute-api.eu-west-1.amazonaws.com/dev/api",
   import: "https://cyg0rwmu8g.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://so2zju5nw2.execute-api.eu-west-1.amazonaws.com/dev/api",
